e8c9ae0ba121b1b764cdeb60efd33421403f2b2e
Romeo-Ndimande <fortune.rndimande@gmail.com>
2024-01-08 19:50:29 -0800

    tasks 3-6

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def new_in_list(my_list, idx, element):
+    tmp = my_list[:]
+    if idx < 0 :
+        return temp
+    if idx > len(my_list) -1:
+        return tmp
+    tmp[idx] = element
+    return tmp
